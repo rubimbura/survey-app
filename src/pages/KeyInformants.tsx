83d40748bc02d90c491ad6d2cf5bdab9cb9ba5e0
abiringira <eabiringira@bk.rw>
2023-07-26 19:45:54 +0200

    Adding key informant array

--- src/pages/KeyInformants.tsx
+++ src/pages/KeyInformants.tsx
@@ -13,3 +13,118 @@ const KeyInformants = () => {
 }
 
 export default KeyInformants
+
+const informantCurrentStatusArr = [
+  {
+      label: 'Current status in general related to your business/ institution/ cooperative',
+      id: 'Current status in general related to your business/ institution/ cooperative'
+    },
+    {
+      label: 'Potential demand for livestock related products',
+      id:   'Potential demand for livestock related products'
+    },
+    {
+      label: 'Competition in Rwandan market/ export market',
+      id: 'Competition in Rwandan market/ export market'
+    },
+    {
+      label: 'Challenges',
+      id: 'Challenges'
+    },
+    {
+      label: 'Opportunities',
+      id: 'Opportunities'
+    },
+]
+
+const informantJobCreationArr = [
+  {
+      label: 'In general',
+      id: 'In general'
+    },
+    {
+      label: 'Special for youth if available',
+      id:   'Special for youth if available'
+    },
+    {
+      label: 'Special for women if available',
+      id: 'Special for women if available'
+    },
+    {
+      label: 'Special for persons with disabilities if available',
+      id: 'Special for persons with disabilities if available'
+    },
+    
+]
+
+const informantPotentialOpportunitiesArr = [
+  {
+      label: 'For new market opportunities',
+      id: 'For new market opportunities'
+    },
+    {
+      label: 'For diversification into other by products',
+      id:   'For diversification into other by products'
+    },
+    {
+      label: 'For distribution strategies in Rwandan market',
+      id: 'For distribution strategies in Rwandan market'
+    },
+    
+]
+
+const informantLiveStockRecomendationsArr = [
+  {
+      label: 'To Farmers',
+      id: 'To Farmers'
+    },
+    {
+      label: 'To MCCs',
+      id:   'To MCCs'
+    },
+    {
+      label: 'Milk Processors',
+      id: 'Milk Processors'
+    },
+
+    {
+      label: 'Meat processors',
+      id: 'Meat processors'
+    },
+    {
+      label: 'Skin processors',
+      id:   'Skin processors'
+    },
+    {
+      label: 'To Traders (milk, meat, eggs)',
+      id: 'To Traders (milk, meat, eggs)'
+    },
+
+    {
+      label: 'To Service providers (vets,…)',
+      id: 'To Service providers (vets,…): '
+    },
+    
+]
+
+const informantStakeHolderContactDetailsArr = [
+
+  {
+    label: 'For Most powerful livestock farmers/producers/ traders',
+    id: 'For Most powerful livestock farmers/producers/ traders'
+  },
+  {
+    label: 'For MCCs and milk transporters',
+    id:   'For MCCs and milk transporters'
+  },
+  {
+    label: 'For Livestock by products processors/ distributors',
+    id: 'For Livestock by products processors/ distributors'
+  },
+
+  {
+    label: 'For Meat processors/distributors',
+    id: 'For Meat processors/distributors'
+  },
+
+  ]
