6860387fa448a728d03027bacb249af48866c6f5
Brian RUBIMBURA <brubimbura@bk.rw>
2023-07-28 12:01:37 +0300

    latest

--- src/pages/KeyInformants.tsx
+++ src/pages/KeyInformants.tsx
@@ -18,47 +18,31 @@ const KeyInformants = () => {
   const [checkedItems, setCheckedItems] = useState(false)
   const [disabled, setDisabled] = useState(false)
 
-  const handletechnicalContraintsChange = (id: any) => {
-    //@ts-ignore
-    mccTechnicalContraintsArr[id].isSelected = !mccTechnicalContraintsArr[id].isSelected
-    setCheckedItems(!checkedItems)
-  }
 
-  const handleStatusChange = (id: number) => {
-    //@ts-ignore
-    informantCurrentStatusArr[id].isSelected = !informantCurrentStatusArr[id].isSelected
-    setCheckedItems(!checkedItems)
-  }
-
-  const handleJobChange = (id: number) => {
-    //@ts-ignore
-    informantJobCreationArr[id].isSelected = !informantJobCreationArr[id].isSelected
-    setCheckedItems(!checkedItems)
-  }
-
-  const handleOpportunities = () => {
-
-  }
 
 
 
   const handleSubmit = () => {
-
-    // const url = 'https://survey-app-heroku-4b2ea8ed2f87.herokuapp.com/information'
-    // axios.post(url, payload)
-    //   .then((response: any) => {
-    //     console.log('Post successful:', response);
-    //   setDisabled(false)
-    //   navigate('/success')
-    //   sessionStorage.removeItem('requireFields')
-    //   })
-    //   .catch((error:any) => {
-    //     console.error('Error posting data:', error);
-    //   setDisabled(false)
-    //   });
-
   }
 
+  
+  const handleChangeForItem = (key:any, event:any) => {
+    const inputValue = event.target.value;
+    setValues((prevValues:any) => ({
+      ...prevValues,
+      [key]: prevValues[key] ? [...prevValues[key], inputValue] : [inputValue],
+    }));
+  };
+
+  const handleChangeForQuestion = (key:any, event:any) => {
+    const inputValue = event.target.value;
+    setValues((prevValues:any) => ({
+      ...prevValues,
+      [key]: prevValues[key] ? [...prevValues[key], inputValue] : [inputValue],
+    }));
+  };
+
+  console.log('the values we have', values)
 
   return (
     <div className="home-page-container">
@@ -84,62 +68,57 @@ const KeyInformants = () => {
           <FormLabel style={{ textTransform: 'uppercase', marginBottom: 30, color: '#67B17B' }} id="demo-row-radio-buttons-group-label">How would you describe the status of livestock value chains in your institution/district/sector?</FormLabel>
         </FormGroup>
 
-          {
-            keyInformantsItems.map(el=> {
-              return(
-                <>
-                  <FormGroup>
-                    <FormLabel style={{ marginBottom: 10, fontWeight: 'bold' }} id="demo-row-radio-buttons-group-label">{el.label}</FormLabel>
-                  </FormGroup>
-                  {
-                    el.items.map( val => {
-                      if(val.label) {
-                        return(
-                          <div>
-                            <InputTextfield placeholder={val.label} handleChange={{}} />
-                          </div>
-                        )
-                      }
-                      if(val.question){
-                        return(
-                          <>
+        {
+          keyInformantsItems.map(el => {
+            return (
+              <>
+                <FormGroup>
+                  <FormLabel style={{ marginBottom: 10, fontWeight: 'bold' }} id="demo-row-radio-buttons-group-label">{el.label}</FormLabel>
+                </FormGroup>
+                {
+                  el.items.map(val => {
+                    if (val.label) {
+                      return (
+                        <div>
+                          <InputTextfield
+                            placeholder={val.label}
+                            handleChange={(event:any) =>
+                              handleChangeForItem(el.value, event)
+                            }
+                          />
+                        </div>
+                      )
+                    }
+                    if (val.question) {
+                      return (
+                        <>
                           <FormGroup>
                             <FormLabel style={{ marginBottom: 10 }} id="demo-row-radio-buttons-group-label">{val.question.label}</FormLabel>
                           </FormGroup>
-                          {val.question.content.map(acc=>{
-                            return(
+                          {val.question.content.map(acc => {
+                            return (
                               <div>
-                            <InputTextfield placeholder={acc.label} handleChange={{}} />
-                          </div>
+                                <InputTextfield 
+                                  placeholder={acc.label} 
+                                  handleChange={(event:any) =>
+                                    handleChangeForQuestion(
+                                      val.question.value,
+                                      event
+                                    )
+                                  }
+                                />
+                              </div>
                             )
                           })}
-                          </>
-                        )
-                      }
-                    })
-                  }
-                </>
-              )
-            })
-          }
-
-
-        {/* <div className="separator-container">
-          <FormGroup>
-            <FormLabel id="demo-row-radio-buttons-group-label"></FormLabel>
-            <div style={{ marginTop: 10 }}></div>
-            {informantCurrentStatusArr.map(el => {
-              return (
-                
-              )
-            })}
-          </FormGroup>
-        </div> */}
-
-
-
-
-
+                        </>
+                      )
+                    }
+                  })
+                }
+              </>
+            )
+          })
+        }
 
 
         <div style={{ display: 'flex', marginLeft: 'auto', width: '30%', marginTop: 30 }}>
@@ -157,177 +136,99 @@ export default KeyInformants
 
 const keyInformantsItems = [
   {
-      label: 'In Dairy Cattle (general current state & livestock farming/production)',
-      items: [
-        {
-          label: 'Current status'
-        },
-        {
-          label: 'Potential demand for livestock related products'
-        },
-        {
-          label: 'Competition in Rwandan market/ export market'
-        },
-        {
-          label: 'Challenges'
-        },
-        {
-          label: 'Opportunities'
-        },
-        {
-          question:{
-            label: 'What are opportunities for job creation in the dairy cattle farming and related businesses?',
-          content:[
+    label: 'In Dairy Cattle (general current state & livestock farming/production)',
+    value:'sampleData',
+    items: [
+      {
+        label: 'Current status',
+      },
+      {
+        label: 'Potential demand for livestock related products',
+      },
+      {
+        label: 'Competition in Rwandan market/ export market',
+      },
+      {
+        label: 'Challenges',
+      },
+      {
+        label: 'Opportunities',
+      },
+      {
+        question: {
+          label: 'What are opportunities for job creation in the dairy cattle farming and related businesses?',
+          value: 'secondSampleValue',
+          content: [
             {
-            label: 'In general: '
-          },
-          {
-            label: 'Youth:'
-          },
-          {
-            label: 'Women:'
-          },
-          {
-            label: 'Persons with disabilities:'
-          }
-        ]
-          } 
-        },
-
-        {
-          question:{
-            label: 'What are the potential opportunities for expanding the dairy cattle farming and related businesses?',
-          content:[
+              label: 'In general: ',
+            },
             {
-            label: 'New market opportunities: '
-          },
-          {
-            label: 'Diversification into other by products'
-          },
-          {
-            label: 'Distribution strategies in Rwandan market'
-          },
-        ]
-          } 
+              label: 'Youth:',
+            },
+            {
+              label: 'Women:',
+            },
+            {
+              label: 'Persons with disabilities:',
+            }
+          ]
         }
-      ]
+      },
+      {
+        question: {
+          label: 'What are opportunities for job creation in the dairy cattle farming and related businesses?',
+          value: 'secondSampleValue',
+          content: [
+            {
+              label: 'In general: ',
+            },
+            {
+              label: 'Youth:',
+            },
+            {
+              label: 'Women:',
+            },
+            {
+              label: 'Persons with disabilities:',
+            }
+          ]
+        }
+      },
+      {
+        question: {
+          label: '9.3.3.	What are the potential opportunities for expanding related to your business/ institution/ cooperative?',
+          value: 'secondSampleValue',
+          content: [
+            {
+              label: 'For new market opportunities:',
+            },
+            {
+              label: 'For diversification into other by products',
+            },
+            {
+              label: 'For distribution strategies in Rwandan market',
+            },
+          ]
+        }
+      },
+      {
+        question: {
+          label: 'What recommendations can you suggest for livestock interventions that would promote job creation at different level of the values chain (reminder to take notes for all value chains (Cattle, Small ruminants, Rabbits, Pig and Poultry):',
+          value: 'secondSampleValue',
+          content: [
+            {
+              label: 'For new market opportunities:',
+            },
+            {
+              label: 'For diversification into other by products',
+            },
+            {
+              label: 'For distribution strategies in Rwandan market',
+            },
+          ]
+        }
+      },
+    ]
   },
 ]
-
-const informantCurrentStatusArr = [
-  {
-    label: 'Current status',
-    id: 'Current status'
-  },
-  {
-    label: 'Potential demand for livestock related products',
-    id: 'Potential demand for livestock related products'
-  },
-  {
-    label: 'Competition in Rwandan market/ export market',
-    id: 'Competition in Rwandan market/ export market'
-  },
-  {
-    label: 'Challenges',
-    id: 'Challenges'
-  },
-  {
-    label: 'Opportunities',
-    id: 'Opportunities'
-  },
-]
-
-const informantJobCreationArr = [
-  {
-    label: 'In general',
-    id: 'In general'
-  },
-  {
-    label: 'Special for youth if available',
-    id: 'Special for youth if available'
-  },
-  {
-    label: 'Special for women if available',
-    id: 'Special for women if available'
-  },
-  {
-    label: 'Special for persons with disabilities if available',
-    id: 'Special for persons with disabilities if available'
-  },
-
-]
-
-const informantPotentialOpportunitiesArr = [
-  {
-    label: 'For new market opportunities',
-    id: 'For new market opportunities'
-  },
-  {
-    label: 'For diversification into other by products',
-    id: 'For diversification into other by products'
-  },
-  {
-    label: 'For distribution strategies in Rwandan market',
-    id: 'For distribution strategies in Rwandan market'
-  },
-
-]
-
-const informantLiveStockRecomendationsArr = [
-  {
-    label: 'To Farmers',
-    id: 'To Farmers'
-  },
-  {
-    label: 'To MCCs',
-    id: 'To MCCs'
-  },
-  {
-    label: 'Milk Processors',
-    id: 'Milk Processors'
-  },
-
-  {
-    label: 'Meat processors',
-    id: 'Meat processors'
-  },
-  {
-    label: 'Skin processors',
-    id: 'Skin processors'
-  },
-  {
-    label: 'To Traders (milk, meat, eggs)',
-    id: 'To Traders (milk, meat, eggs)'
-  },
-
-  {
-    label: 'To Service providers (vets,…)',
-    id: 'To Service providers (vets,…): '
-  },
-
-]
-
-const informantStakeHolderContactDetailsArr = [
-
-  {
-    label: 'For Most powerful livestock farmers/producers/ traders',
-    id: 'For Most powerful livestock farmers/producers/ traders'
-  },
-  {
-    label: 'For MCCs and milk transporters',
-    id: 'For MCCs and milk transporters'
-  },
-  {
-    label: 'For Livestock by products processors/ distributors',
-    id: 'For Livestock by products processors/ distributors'
-  },
-
-  {
-    label: 'For Meat processors/distributors',
-    id: 'For Meat processors/distributors'
-  },
-
-]
-
-
 
