3b9e4f4089684c5c2890b1ac7c7f1a92c98d9fa0
abiringira <eabiringira@bk.rw>
2023-08-04 10:57:05 +0200

    Removing the page as we are waiting to be paid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -216,167 +216,169 @@ const Home = () => {
   }
 
   return (
-    <div className="home-page-container">
-      <HeaderNav />
-      <div style={{ padding: 32 }}>
-        <div style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
-          <DatePickerComponent
-            placeholder="Date of interview"
-            handleDateChange={handleDateChange}
-            required={true}
-            error={errors.date}
-            errorText={helperText.date}
-          />
-        </div>
-        <div className="row-item-ctn">
-          <InputTextfield
-            placeholder="Province"
-            handleChange={(e: any) => setValues({
-              ...values,
-              province: e.target.value
-            })}
-            required={true}
-            error={errors.province}
-            errorText={helperText.province}
-          />
-          <InputTextfield
-            placeholder="District"
-            handleChange={(e: any) => setValues({
-              ...values,
-              district: e.target.value
-            })}
-            error={errors.district}
-            errorText={helperText.district}
-          />
-        </div>
-        <div className="row-item-ctn">
-          <InputTextfield
-            placeholder="Sector"
-            handleChange={(e: any) => setValues({
-              ...values,
-              sector: e.target.value
-            })}
-            error={errors.sector}
-            errorText={helperText.sector}
-          />
-          <InputTextfield
-            placeholder="Village"
-            handleChange={(e: any) => setValues({
-              ...values,
-              village: e.target.value
-            })}
-            error={errors.village}
-            errorText={helperText.village}
-          />
-        </div>
-        <div className="row-item-ctn">
-        <InputTextfield
-            placeholder="Names"
-            handleChange={(e: any) => setValues({
-              ...values,
-              name: e.target.value
-            })}
-            error={errors.name}
-            errorText={helperText.name}
-          />
-          <InputTextfield
-            placeholder="Age"
-            handleChange={(e: any) => setValues({
-              ...values,
-              age: e.target.value
-            })}
-            error={errors.age}
-            errorText={helperText.age}
-          />
-        </div>
-        <div className="row-item-ctn">
-        <InputTextfield
-            placeholder="Contact"
-            handleChange={(e: any) => setValues({
-              ...values,
-              contact: e.target.value
-            })}
-            error={errors.contact}
-            errorText={helperText.contact}
-          />
-          <SelectField
-            placeholder="Gender"
-            menuItems={genderArr}
-            value={values.gender}
-            handleChange={(event: any) => {
-              setValues({
-                ...values,
-                gender: event.target.value,
-              })
-            }}
+   
+    // <div className="home-page-container">
+    //   <HeaderNav />
+    //   <div style={{ padding: 32 }}>
+    //     <div style={{ display: 'flex', alignItems: 'center', gap: 20 }}>
+    //       <DatePickerComponent
+    //         placeholder="Date of interview"
+    //         handleDateChange={handleDateChange}
+    //         required={true}
+    //         error={errors.date}
+    //         errorText={helperText.date}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //       <InputTextfield
+    //         placeholder="Province"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           province: e.target.value
+    //         })}
+    //         required={true}
+    //         error={errors.province}
+    //         errorText={helperText.province}
+    //       />
+    //       <InputTextfield
+    //         placeholder="District"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           district: e.target.value
+    //         })}
+    //         error={errors.district}
+    //         errorText={helperText.district}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //       <InputTextfield
+    //         placeholder="Sector"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           sector: e.target.value
+    //         })}
+    //         error={errors.sector}
+    //         errorText={helperText.sector}
+    //       />
+    //       <InputTextfield
+    //         placeholder="Village"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           village: e.target.value
+    //         })}
+    //         error={errors.village}
+    //         errorText={helperText.village}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //     <InputTextfield
+    //         placeholder="Names"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           name: e.target.value
+    //         })}
+    //         error={errors.name}
+    //         errorText={helperText.name}
+    //       />
+    //       <InputTextfield
+    //         placeholder="Age"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           age: e.target.value
+    //         })}
+    //         error={errors.age}
+    //         errorText={helperText.age}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //     <InputTextfield
+    //         placeholder="Contact"
+    //         handleChange={(e: any) => setValues({
+    //           ...values,
+    //           contact: e.target.value
+    //         })}
+    //         error={errors.contact}
+    //         errorText={helperText.contact}
+    //       />
+    //       <SelectField
+    //         placeholder="Gender"
+    //         menuItems={genderArr}
+    //         value={values.gender}
+    //         handleChange={(event: any) => {
+    //           setValues({
+    //             ...values,
+    //             gender: event.target.value,
+    //           })
+    //         }}
             
-          />
-        </div>
-        <div className="row-item-ctn">
-        <SelectField
-            placeholder="Level of education"
-            menuItems={educationArr}
-            value={values.education}
-            handleChange={(event: any) => {
-              setValues({
-                ...values,
-                qualification: event.target.value,
-              })
-            }}
-          />
-          <SelectField
-            placeholder="Disablity"
-            menuItems={disablityItems}
-            value={values.disability}
-            handleChange={(event: any) => {
-              setValues({
-                ...values,
-                disability: event.target.value,
-              })
-            }}
-          />
-        </div>
-        <div className="row-item-ctn">
-        <SelectField
-            placeholder="Occupation"
-            menuItems={ocupationArr}
-            value={values.occupation}
-            handleChange={(event: any) => {
-              setValues({
-                ...values,
-                occupation: event.target.value,
-              })
-            }}
-          />
-        </div>
-        <div className="row-item-ctn">
-          <InputTextfield 
-            placeholder="What is your position in your business/organization/institution/cooperative" 
-            handleChange={(event:any) => 
-              setValues({
-              ...values,
-              position:event.target.value
-            })} 
-            error={errors.position}
-            errorText={helperText.position}
-        />
-          <InputTextfield 
-            placeholder="Number of years involved in mentioned occupation" 
-            handleChange={(event:any) => {
-              setValues({
-                ...values,
-                experience:event.target.value
-              })
-            }} 
-            error={errors.experience}
-            errorText={helperText.experience}
-          />
-        </div>
-        <div style={{ display: 'flex', marginLeft: 'auto', width: '30%', marginTop: 30 }}>
-          <PrimaryButton onClick={handleSubmit} label="Next" />
-        </div>
-      </div>
-    </div>
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //     <SelectField
+    //         placeholder="Level of education"
+    //         menuItems={educationArr}
+    //         value={values.education}
+    //         handleChange={(event: any) => {
+    //           setValues({
+    //             ...values,
+    //             qualification: event.target.value,
+    //           })
+    //         }}
+    //       />
+    //       <SelectField
+    //         placeholder="Disablity"
+    //         menuItems={disablityItems}
+    //         value={values.disability}
+    //         handleChange={(event: any) => {
+    //           setValues({
+    //             ...values,
+    //             disability: event.target.value,
+    //           })
+    //         }}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //     <SelectField
+    //         placeholder="Occupation"
+    //         menuItems={ocupationArr}
+    //         value={values.occupation}
+    //         handleChange={(event: any) => {
+    //           setValues({
+    //             ...values,
+    //             occupation: event.target.value,
+    //           })
+    //         }}
+    //       />
+    //     </div>
+    //     <div className="row-item-ctn">
+    //       <InputTextfield 
+    //         placeholder="What is your position in your business/organization/institution/cooperative" 
+    //         handleChange={(event:any) => 
+    //           setValues({
+    //           ...values,
+    //           position:event.target.value
+    //         })} 
+    //         error={errors.position}
+    //         errorText={helperText.position}
+    //     />
+    //       <InputTextfield 
+    //         placeholder="Number of years involved in mentioned occupation" 
+    //         handleChange={(event:any) => {
+    //           setValues({
+    //             ...values,
+    //             experience:event.target.value
+    //           })
+    //         }} 
+    //         error={errors.experience}
+    //         errorText={helperText.experience}
+    //       />
+    //     </div>
+    //     <div style={{ display: 'flex', marginLeft: 'auto', width: '30%', marginTop: 30 }}>
+    //       <PrimaryButton onClick={handleSubmit} label="Next" />
+    //     </div>
+    //   </div>
+    // </div>
+    <div></div>
   )
 }
 
